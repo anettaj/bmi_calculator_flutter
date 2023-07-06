6ccf0068b702886748d36510353d9906e1147d6a
anettaj <58222128+anettaj@users.noreply.github.com>
2023-07-06 13:38:25 +0530

    Update main.dart

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -12,7 +12,7 @@ class BMICalculator extends StatelessWidget {
 
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor : Color(0xFF0A0E21),
         colorScheme: ColorScheme.light(
           primary:Color(0xFF0A0E21),
 
