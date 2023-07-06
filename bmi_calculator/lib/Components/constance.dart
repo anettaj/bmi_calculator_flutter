83f72622223585d710392cf8189a68f8c44efae2
anettaj <58222128+anettaj@users.noreply.github.com>
2023-07-06 09:46:27 +0530

    Create constance.dart

---
+++ bmi_calculator/lib/Components/constance.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+const kBottomContainerHeight=80.0;
+const kActiveCardColour=Color(0xFF1D1E33);
+const kInactiveCardColour=Color(0xFF111328);
+const kBottomContainerColour=Color(0xffEB1555);
+
+const kLabel_textstyle=TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8D8E98),
+);
+
+const kNumber_Style= TextStyle(
+fontSize: 50.0,
+fontWeight: FontWeight.w900,
+);
+
+const kBottomLabel_textstyle=TextStyle(
+  fontSize: 25.0,
+  color:Colors.white,
+  fontWeight: FontWeight.bold,
+);
+const kTitleTextStyle =TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 50.0,
+);
+const kResultTextStyle =TextStyle(
+  color: Color(0xff24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+const kResultNumberStyle =TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
